0c1363bd7df8d20fdbc4e79fda3cac976837695a
Эль шейх <elina_shibanova@mail.ru>
2025-05-05 21:26:12 +0300

    Update

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -5,6 +5,23 @@
 template<typename T>
 class TPQueue {
   // реализация шаблона очереди с приоритетом на связанном списке
+ private:
+    std::list<T> list;
+
+ public:
+    void push(const T& item) {
+        auto it = list.begin();
+        while (it != list.end() && it->prior >= item.prior) {
+            ++it;
+        }
+        list.insert(it, item);
+    }
+
+    T pop() {
+        T item = list.front();
+        list.pop_front();
+        return item;
+    }
 };
 
 struct SYM {
